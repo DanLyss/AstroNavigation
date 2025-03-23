19a3329c2ae7fa1b29871e24c2dbef8a49acff97
kefolen <artem.telkov05@gmail.com>
2025-03-24 00:18:57 +0100

    Added unittests for math2

--- backend/math1/unittests_reverse_projection.py
+++ backend/math1/unittests_reverse_projection.py
@@ -1,8 +1,8 @@
 import unittest
 import numpy as np
 import matplotlib.pyplot as plt
-from converter_flat_hor import Star, Star_Cluster
-
+from ..math1.converter_flat_hor import Star, Star_Cluster
+from ..math2.latt_long_calc import mean_latitude, norm
 #NOTE: DATE OF PHOTOS - 9/11/2001, location - Bremen, Bremen time 22:00
 
 
@@ -151,6 +151,34 @@ class TestProjectionOnDatasets(unittest.TestCase):
                 "rotation_angle_deg": 24.7
             }
         ]
+    def test_lat_az(self):
+        for idx, case in enumerate(self.test_cases):
+            with self.subTest(f"Test Case #{idx + 1}"):
+                stars = []
+                for i in range(len(case["pixel_coords"])):
+                    x, y = case["pixel_coords"][i]
+                    ra_h, ra_m, ra_s = case["RA_Dec"][i][0]
+                    dec_d, dec_m, dec_s = case["RA_Dec"][i][1]
+
+                    ra_deg = (ra_h * 3600 + ra_m * 60 + ra_s) / 86400 * 360
+                    dec_deg = dec_d + dec_m / 60 + dec_s / 3600
+
+                    stars.append(Star(x, y, np.deg2rad(ra_deg), np.deg2rad(dec_deg)))
+
+                pos_angle_rad = np.deg2rad(case["positional_angle_deg"])
+                rotation_angle = np.deg2rad(case["rotation_angle_deg"])
+                cluster = Star_Cluster(stars, pos_angle_rad, rotation_angle)
+                true_vals = np.array([np.deg2rad(case["AzAlt_deg"][0][0]), np.deg2rad(53.16542)])
+
+                computed_vals1 = np.array(mean_latitude(cluster)[:-1])
+                computed_vals2 = np.array([norm(computed_vals1[0]-np.pi, 0, 2 * np.pi), computed_vals1[1]])
+
+                error = min(np.mean(np.abs(true_vals - computed_vals1)), np.mean(np.abs(true_vals - computed_vals2)))
+                self.assertLess(error,
+                                0.01,
+                                f"Latt_Az test failed (mean error: {np.rad2deg(error):.2f}°)")
+
+                print(f"[Latt Test Case {idx + 1}] Lattitude error: {np.rad2deg(min(np.abs(true_vals[1] - computed_vals1[1]), np.abs(true_vals[1] - computed_vals2[1]))):.5f}°")
 
     def test_all_cases(self):
         for idx, case in enumerate(self.test_cases):
